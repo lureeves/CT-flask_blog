f6824754070b642734ee7734d237316cc0c57574
icncode <ljr77777@gmail.com>
2023-03-21 13:21:08 -0700

    Added sign up form

--- app/routes.py
+++ app/routes.py
@@ -8,9 +8,20 @@ def index():
     return render_template('index.html', posts=posts, logged_in = False)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
+    # Create an instance of the form (in the context of the current request)
     form = SignUpForm()
+    # Check if the form was submitted and that all of the fields are valid 
+    if form.validate_on_submit():
+        # If so, get the data from the form fields
+        print('Hooray your form was validated!')
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(first_name, last_name, email, username, password)
     return render_template('signup.html', form=form)
 
 
